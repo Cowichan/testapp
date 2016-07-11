f8038d1f5c36fa9cfaeecdba2a84e690807c4902
Cowichan <contactopenwebsites@gmail.com>
2016-07-11 17:21:22 +0200

    ajouter le modele de produit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get "/team" => "pages#team"
+  get "/contact" => "pages#join_us"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
